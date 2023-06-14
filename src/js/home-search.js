4c514ae296438be6b9c359d1ccb52a53a35cda21
Burke Atkerson <burkeatkerson@gmail.com>
2023-06-14 13:54:16 -0600

    Project complete. Fully functional website.

--- src/js/home-search.js
+++
@@ -1,9 +0,0 @@
-<!--Home Search API for PPAR -->
-<!-- ** DO NOT DEFER ** This must load at the top of the page, e.g. in the page head. -->
-<script src="https://www.mbb2.com/version3/css/theme/acid/sXHB23E"></script>
-var MBB = {seo : "false",data:{ acid : "sXHB23E" } };
-function mbbMapLoaded(){ MBB.googleMaps = true; };
-</script>
-<!-- NOTE: Google MAP API must load AFTER the declaration of mbbMapLoaded(), and BEFORE my-buying-buddy.5.0.js.gz -->
-<script src="https://maps.googleapis.com/maps/api/js?callback=mbbMapLoaded&libraries=places&key=AIzaSyCC85ouh7iCFalu9c05El1N1tDV3vQ9YVE"></script>
-<script src="https://d2w6u17ngtanmy.cloudfront.net/scripts/my-buying-buddy.5.0.js.gz"></script>
